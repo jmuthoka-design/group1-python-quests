88d08114ae434b6774dbde5c6bed11a096ffa0a7
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-08-24 05:13:41 -0700

    Meet a superhero

---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+hero_name = "Elastagirl"
+hero_power = "the ability to stretch her body and conform to any desirable shape" 
+print(f"Meet {hero_name}, a hero known for {hero_power}.")
