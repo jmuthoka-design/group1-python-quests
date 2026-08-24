cdd13e3668350f24c87b4042310843489224deba
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-24 05:08:40 -0700

    quest13

---
+++ quests/quest_13_maze_of_many_choices.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+score = int(input("Enter your score (0-100): "))
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("Needs Improvement")
