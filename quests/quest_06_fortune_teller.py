98df35430c313f638a42144dd28d2b4f88726660
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-08-25 02:22:57 -0700

    name and quest you are pursuing

---
+++ quests/quest_06_fortune_teller.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+user_answer=input("What is your name, adventurer?")
+user_quest=input("What is your quest?")
+print(f"{user_answer} I see you are cruising nicely with your {user_quest} quest! Hope you are having a blast!!")
